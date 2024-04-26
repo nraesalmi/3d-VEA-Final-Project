fdcd79e14abcdbf8207ebcbce1a0bde6df1e7da4
BuiDaiHieu <91104220+BuiDaiHieu@users.noreply.github.com>
2024-04-26 23:44:47 +0300

    Update EnemyAiTutorial.cs

--- Scripts/EnemyAiTutorial.cs
+++ Scripts/EnemyAiTutorial.cs
@@ -1,11 +1,14 @@
 ﻿
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class EnemyAiTutorial : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Transform player;
+    public Transform currentLocation;
     public LayerMask whatIsGround, whatIsPlayer;
     public float health;
 
@@ -22,12 +25,13 @@ public class EnemyAiTutorial : MonoBehaviour
     //States
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
+    public float maxTimeOutOfSight = 6.0f;
+    public Transform[] teleportLocations;
+    public float distantThreshold = 3.0f;
 
-    private void Start()
-    {
-        player = GameObject.Find("PlayerObj").transform;
-        agent = GetComponent<NavMeshAgent>();
-    }
+    private float timeOutOfSight = 0.0f;
+    private bool canSeePlayer = false;
+    private bool hasTeleported = false;
 
     private void Update()
     {
@@ -35,23 +39,52 @@ public class EnemyAiTutorial : MonoBehaviour
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
 
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        if (playerInSightRange) {
+            if (playerInAttackRange) {
+                AttackPlayer();
+            } else {
+                canSeePlayer = true;
+                timeOutOfSight = 0.0f;
+                ChasePlayer();
+            } 
+        } 
+        else {
+            canSeePlayer = false;
+            timeOutOfSight += Time.deltaTime;
+
+            if (timeOutOfSight >= maxTimeOutOfSight)
+            {
+                Teleport();
+                timeOutOfSight = 0.0f; 
+            }
+        }
+
+        Patroling();
     }
 
     private void Patroling()
     {
-        if (!walkPointSet) SearchWalkPoint();
+        if (hasTeleported == true) {
+            walkPointSet = false;
+            hasTeleported = false;
+        }
 
-        if (walkPointSet)
+        if (!walkPointSet) 
+        {
+            SearchWalkPoint();
+        } 
+        else
+        {
             agent.SetDestination(walkPoint);
+        }
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
 
         //Walkpoint reached
         if (distanceToWalkPoint.magnitude < 1f)
+        {
             walkPointSet = false;
+        }
     }
     private void SearchWalkPoint()
     {
@@ -62,7 +95,9 @@ public class EnemyAiTutorial : MonoBehaviour
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
             walkPointSet = true;
+        }
     }
 
     private void ChasePlayer()
@@ -111,5 +146,22 @@ public class EnemyAiTutorial : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private void Teleport() {
+        bool checkDistance = true;
+        while (checkDistance) {
+            int index = Random.Range(0, teleportLocations.Length);
+            Transform randomLocation = teleportLocations[index];
+            Vector3 distanceToPlayerVector = randomLocation.position - player.position;
+            float distanceToPlayer = distanceToPlayerVector.magnitude;
+
+            if (distanceToPlayer > distantThreshold && currentLocation.position != randomLocation.position) {
+                agent.Warp(randomLocation.position);
+                currentLocation.position = randomLocation.position;
+                checkDistance = false;
+            }
+        }
+        hasTeleported = true;
     }
 }
